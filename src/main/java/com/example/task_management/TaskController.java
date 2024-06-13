b7fe11ffdab0d5e6bca0b447c3b61cc9e3a57d24
shankar <coder.shankar@gmail.com>
2024-06-13 19:24:11 +0545

    add controller and service to manage task

---
+++ src/main/java/com/example/task_management/TaskController.java
@@ -0,0 +1,50 @@
+package com.example.task_management;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<TaskDto> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDto> getTaskById(@PathVariable Long id) {
+        TaskDto task = taskService.getTaskById(id);
+        return ResponseEntity.ok(task);
+    }
+
+    @PostMapping
+    public TaskDto createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDto> updateTask(@PathVariable Long id, @RequestBody TaskDto taskDetails) {
+        TaskDto updatedTask = taskService.updateTask(id, taskDetails);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/example/task_management/TaskService.java
@@ -0,0 +1,54 @@
+package com.example.task_management;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<TaskDto> getAllTasks() {
+        return taskRepository.findAll()
+                             .stream().map(this::toTaskDto)
+                             .toList();
+    }
+
+    public TaskDto getTaskById(Long id) {
+        return taskRepository.findById(id)
+                             .map(this::toTaskDto)
+                             .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public TaskDto createTask(Task task) {
+        Task save = taskRepository.save(task);
+        return toTaskDto(save);
+    }
+
+    public TaskDto updateTask(Long id, TaskDto taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        Task updated = taskRepository.save(task);
+        return toTaskDto(updated);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    private TaskDto toTaskDto(Task e) {
+        TaskDto dto = new TaskDto();
+        dto.setId(e.getId());
+        dto.setTitle(e.getTitle());
+        dto.setDescription(e.getDescription());
+        dto.setCreatedAt(e.getCreatedAt());
+
+        return dto;
+    }
+}
